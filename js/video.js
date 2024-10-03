ffb9bf8724e40ee4c2b14fab8e2eaa9fc37b28bd
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-04 02:22:53 +0600

    data load nav

--- js/video.js
+++ js/video.js
@@ -3,14 +3,21 @@
 const loadCategories =() =>{
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
     .then(res =>res.json())
-    .then(data => displayCategories(data.categories
-    ))
+    .then(data => displayCategories(data.categories))
     .catch(error => console.log(error))
 }
 
 
-const displayCategories =(data) =>{
-    console.log(data)
+const displayCategories =(categories) =>{
+    const categoryContiner =document.getElementById('categorys')
+    // console.log(data)
+    categories.forEach(item =>{
+        // console.log(item)
+        const button =document.createElement('button');
+        button.classList='btn';
+        button.innerText =item.category;
+        categoryContiner.append(button)
+    })
 }
 
 
